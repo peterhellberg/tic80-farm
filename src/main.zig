d9520f55e8615b1eea4b8fd03653a769f41a36ea
Peter Hellberg <peter@c7.se>
2023-12-05 15:02:56 +0100

    src: Mouse control

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,130 @@
 const std = @import("std");
 const tic = @import("tic");
 
+const Farm = struct {
+    bot: Bot = .{},
+    inventory: Inventory = .{},
+    plots: [16 * 30]Plot = undefined,
+
+    fn start(self: *Farm) void {
+        for (1..17) |y| {
+            for (0..30) |x| {
+                self.plots[offset(x, y)] = Plot.new(@intCast(x), @intCast(y));
+            }
+        }
+    }
+
+    fn update(self: *Farm) void {
+        self.bot.update();
+
+        if (tic.pressed(4) or self.bot.act()) self.action();
+
+        for (&self.plots) |*p| {
+            p.update();
+        }
+    }
+
+    fn draw(self: *Farm) void {
+        tic.cls(0);
+        tic.map(.{});
+        self.inventory.draw();
+        self.bot.draw();
+    }
+
+    fn action(self: *Farm) void {
+        const p = self.plot();
+
+        switch (tic.mget(p.x, p.y)) {
+            0, 1 => self.till(p),
+            2 => self.plant(p),
+            3 => self.water(p),
+            7, 8, 9 => self.pick(p),
+            else => {},
+        }
+    }
+
+    fn till(_: *Farm, p: *Plot) void {
+        p.state(.tilled);
+    }
+
+    fn plant(self: *Farm, p: *Plot) void {
+        if (self.inventory.seeds > 0) {
+            p.state(.planted);
+            self.inventory.seeds -|= 1;
+        } else if (self.inventory.carrots > 0) {
+            p.state(.growing);
+            self.inventory.carrots -|= 1;
+        }
+    }
+
+    fn water(_: *Farm, p: *Plot) void {
+        p.state(.watered);
+    }
+
+    fn pick(self: *Farm, p: *Plot) void {
+        switch (p.s) {
+            .grown => self.inventory.carrots +|= 1,
+            .flowering => self.inventory.seeds +|= 2,
+            else => {},
+        }
+
+        p.state(.empty);
+    }
+
+    fn plot(self: *Farm) *Plot {
+        return &self.plots[offset(self.bot.x, self.bot.y)];
+    }
+};
+
+var farm = Farm{};
+
+const Bot = struct {
+    x: u8 = 14,
+    y: u8 = 8,
+
+    mouse: Mouse = .{},
+
+    fn update(b: *Bot) void {
+        b.mouse.update();
+
+        if (b.mouse.leftHeld()) {
+            if (b.mouse.x > b.x) b.x +|= 1;
+            if (b.mouse.x < b.x) b.x -|= 1;
+            if (b.mouse.y > b.y) b.y +|= 1;
+            if (b.mouse.y < b.y) b.y -|= 1;
+        }
+
+        if (tic.pressed(0) and b.y > 1) b.y -|= 1;
+        if (tic.pressed(1) and b.y < 16) b.y +|= 1;
+        if (tic.pressed(2)) b.x -|= 1;
+        if (tic.pressed(3) and b.x < 29) b.x +|= 1;
+    }
+
+    fn act(b: *Bot) bool {
+        return (b.x == b.mouse.x and b.y == b.mouse.y) and b.mouse.rightReleased();
+    }
+
+    fn draw(b: *Bot) void {
+        if (b.mouse.leftHeld()) {
+            tic.rectb(b.mouse.x * 8, b.mouse.y * 8, 8, 8, 15);
+        }
+
+        spr(15, b.x, b.y, .{ .transparent = &[_]u8{0} });
+    }
+};
+
+const Inventory = struct {
+    seeds: u16 = 3,
+    carrots: u16 = 0,
+
+    fn draw(self: *Inventory) void {
+        spr(19, 22, 0, .{});
+        printf(" {d}", .{self.seeds}, 23, 0, .{ .small_font = true, .color = 2 });
+        spr(18, 26, 0, .{});
+        printf(" {d}", .{self.carrots}, 27, 0, .{ .small_font = true, .color = 2 });
+    }
+};
+
 const State = enum {
     empty,
     tilled,
@@ -63,108 +187,42 @@ const Plot = struct {
     }
 };
 
-const Inventory = struct {
-    seeds: u16 = 3,
-    carrots: u16 = 0,
+const Mouse = struct {
+    x: u8 = 0,
+    y: u8 = 0,
 
-    fn draw(self: *Inventory) void {
-        spr(19, 22, 0, .{});
-        printf(" {d}", .{self.seeds}, 23, 0, .{ .small_font = true, .color = 2 });
-        spr(18, 26, 0, .{});
-        printf(" {d}", .{self.carrots}, 27, 0, .{ .small_font = true, .color = 2 });
-    }
-};
+    data: tic.MouseData = .{},
+    prev: tic.MouseData = .{},
 
-const Farm = struct {
-    bot: Bot = .{},
-    inventory: Inventory = .{},
-    plots: [16 * 30]Plot = undefined,
+    fn update(self: *Mouse) void {
+        self.prev = self.data;
+        tic.mouse(&self.data);
 
-    fn start(self: *Farm) void {
-        for (1..17) |y| {
-            for (0..30) |x| {
-                self.plots[offset(x, y)] = Plot.new(@intCast(x), @intCast(y));
-            }
-        }
+        if (self.data.x >= 0 and self.data.x <= tic.WIDTH)
+            self.x = @intCast(@divFloor(self.data.x, 8));
+
+        if (self.data.y >= 0 and self.data.y <= tic.HEIGHT)
+            self.y = @intCast(@divFloor(self.data.y, 8));
     }
 
-    fn update(self: *Farm) void {
-        if (tic.pressed(4)) self.action();
-
-        self.bot.update();
-
-        for (&self.plots) |*p| {
-            p.update();
-        }
+    fn leftPressed(self: *Mouse) bool {
+        return self.data.left and !self.prev.left;
     }
 
-    fn draw(self: *Farm) void {
-        tic.cls(0);
-        tic.map(.{});
-        self.inventory.draw();
-        self.bot.draw();
+    fn leftHeld(self: *Mouse) bool {
+        return self.data.left and self.prev.left;
     }
 
-    fn action(self: *Farm) void {
-        const p = self.plot();
-
-        switch (tic.mget(p.x, p.y)) {
-            0, 1 => self.till(p),
-            2 => self.plant(p),
-            3 => self.water(p),
-            7, 8, 9 => self.pick(p),
-            else => {},
-        }
+    fn leftReleased(self: *Mouse) bool {
+        return !self.data.left and self.prev.left;
     }
 
-    fn till(_: *Farm, p: *Plot) void {
-        p.state(.tilled);
+    fn rightHeld(self: *Mouse) bool {
+        return self.data.right and self.prev.right;
     }
 
-    fn plant(self: *Farm, p: *Plot) void {
-        if (self.inventory.seeds > 0) {
-            p.state(.planted);
-            self.inventory.seeds -|= 1;
-        } else if (self.inventory.carrots > 0) {
-            p.state(.grown);
-            self.inventory.carrots -|= 1;
-        }
-    }
-
-    fn water(_: *Farm, p: *Plot) void {
-        p.state(.watered);
-    }
-
-    fn pick(self: *Farm, p: *Plot) void {
-        switch (p.s) {
-            .grown => self.inventory.carrots +|= 1,
-            .flowering => self.inventory.seeds +|= 2,
-            else => {},
-        }
-
-        p.state(.empty);
-    }
-
-    fn plot(self: *Farm) *Plot {
-        return &self.plots[offset(self.bot.x, self.bot.y)];
-    }
-};
-
-var farm = Farm{};
-
-const Bot = struct {
-    x: u8 = 14,
-    y: u8 = 8,
-
-    fn update(b: *Bot) void {
-        if (tic.pressed(0) and b.y > 1) b.y -|= 1;
-        if (tic.pressed(1) and b.y < 16) b.y +|= 1;
-        if (tic.pressed(2)) b.x -|= 1;
-        if (tic.pressed(3) and b.x < 29) b.x +|= 1;
-    }
-
-    fn draw(b: *Bot) void {
-        spr(15, b.x, b.y, .{ .transparent = &[_]u8{0} });
+    fn rightReleased(self: *Mouse) bool {
+        return !self.data.right and self.prev.right;
     }
 };
 
