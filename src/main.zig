07cf4e7517578910bec332cb123df4be78832582
Peter Hellberg <peter@c7.se>
2023-12-05 14:27:05 +0100

    src: Farmable plots

--- src/main.zig
+++ src/main.zig
@@ -1,88 +1,152 @@
+const std = @import("std");
 const tic = @import("tic");
+
+const State = enum {
+    empty,
+    tilled,
+    planted,
+    watered,
+    growing,
+    grown,
+    flowering,
+};
+
+const Plot = struct {
+    s: State = .empty,
+    x: u8 = 0,
+    y: u8 = 0,
+    g: u32 = 0,
+
+    fn new(x: u8, y: u8) Plot {
+        return .{
+            .x = x,
+            .y = y,
+        };
+    }
+
+    fn update(self: *Plot) void {
+        self.g += 1;
+
+        if (self.g < 300) return;
+
+        const x: i32 = @intCast(self.x);
+        const y: i32 = @intCast(self.y);
+
+        switch (tic.mget(x, y)) {
+            4 => self.state(.growing),
+            5 => self.mset(6),
+            6 => self.state(.grown),
+            7 => self.mset(8),
+            8 => self.state(.flowering),
+            else => {},
+        }
+
+        self.g = 0;
+    }
+
+    fn state(self: *Plot, s: State) void {
+        self.s = s;
+
+        switch (self.s) {
+            .empty => self.mset(0),
+            .tilled => self.mset(2),
+            .planted => self.mset(3),
+            .watered => self.mset(4),
+            .growing => self.mset(5),
+            .grown => self.mset(7),
+            .flowering => self.mset(9),
+        }
+    }
+
+    fn mset(self: *Plot, tile_id: u32) void {
+        tic.mset(self.x, self.y, tile_id);
+    }
+};
+
+const Inventory = struct {
+    seeds: u16 = 3,
+    carrots: u16 = 0,
+
+    fn draw(self: *Inventory) void {
+        spr(19, 22, 0, .{});
+        printf(" {d}", .{self.seeds}, 23, 0, .{ .small_font = true, .color = 2 });
+        spr(18, 26, 0, .{});
+        printf(" {d}", .{self.carrots}, 27, 0, .{ .small_font = true, .color = 2 });
+    }
+};
 
 const Farm = struct {
     bot: Bot = .{},
+    inventory: Inventory = .{},
+    plots: [16 * 30]Plot = undefined,
 
-    seeds: u16 = 3,
-    carrots: u16 = 0,
-    growth: u16 = 0,
+    fn start(self: *Farm) void {
+        for (1..17) |y| {
+            for (0..30) |x| {
+                self.plots[offset(x, y)] = Plot.new(@intCast(x), @intCast(y));
+            }
+        }
+    }
 
     fn update(self: *Farm) void {
         if (tic.pressed(4)) self.action();
 
         self.bot.update();
 
-        self.grow();
-    }
-
-    fn grow(self: *Farm) void {
-        self.growth += 1;
-
-        if (self.growth < 300) return;
-
-        for (0..30) |X| {
-            for (0..17) |Y| {
-                const x: i32 = @intCast(X);
-                const y: i32 = @intCast(Y);
-
-                switch (tic.mget(x, y)) {
-                    4 => tic.mset(x, y, 5),
-                    5 => tic.mset(x, y, 6),
-                    6 => tic.mset(x, y, 7),
-                    else => {},
-                }
-            }
+        for (&self.plots) |*p| {
+            p.update();
         }
-
-        self.growth = 0;
     }
 
     fn draw(self: *Farm) void {
         tic.cls(0);
         tic.map(.{});
-        self.status();
+        self.inventory.draw();
         self.bot.draw();
     }
 
-    fn status(self: *Farm) void {
-        spr(19, 22, 0, .{});
-        printf(" {d}", .{self.seeds}, 23, 0, .{ .small_font = true, .color = 2 });
-        spr(18, 26, 0, .{});
-        printf(" {d}", .{self.carrots}, 27, 0, .{ .small_font = true, .color = 2 });
-    }
-
     fn action(self: *Farm) void {
-        const x = self.bot.x;
-        const y = self.bot.y;
+        const p = self.plot();
 
-        switch (tic.mget(x, y)) {
-            0, 1 => self.till(),
-            2 => self.plant(),
-            3 => self.water(),
-            7 => self.pick(),
+        switch (tic.mget(p.x, p.y)) {
+            0, 1 => self.till(p),
+            2 => self.plant(p),
+            3 => self.water(p),
+            7, 8, 9 => self.pick(p),
             else => {},
         }
     }
 
-    fn till(self: *Farm) void {
-        tic.mset(self.bot.x, self.bot.y, 2);
+    fn till(_: *Farm, p: *Plot) void {
+        p.state(.tilled);
     }
 
-    fn plant(self: *Farm) void {
-        if (self.seeds > 0) {
-            tic.mset(self.bot.x, self.bot.y, 3);
-            self.seeds -|= 1;
+    fn plant(self: *Farm, p: *Plot) void {
+        if (self.inventory.seeds > 0) {
+            p.state(.planted);
+            self.inventory.seeds -|= 1;
+        } else if (self.inventory.carrots > 0) {
+            p.state(.grown);
+            self.inventory.carrots -|= 1;
         }
     }
 
-    fn water(self: *Farm) void {
-        tic.mset(self.bot.x, self.bot.y, 4);
+    fn water(_: *Farm, p: *Plot) void {
+        p.state(.watered);
     }
 
-    fn pick(self: *Farm) void {
-        tic.mset(self.bot.x, self.bot.y, 2);
-        self.carrots +|= 1;
-        self.seeds +|= 3;
+    fn pick(self: *Farm, p: *Plot) void {
+        switch (p.s) {
+            .grown => self.inventory.carrots +|= 1,
+            .flowering => self.inventory.seeds +|= 2,
+            else => {},
+        }
+
+        p.state(.empty);
+    }
+
+    fn plot(self: *Farm) *Plot {
+        return &self.plots[offset(self.bot.x, self.bot.y)];
     }
 };
 
@@ -110,6 +174,14 @@ fn printf(comptime fmt: []const u8, fmtargs: anytype, x: i32, y: i32, args: tic.
 
 fn spr(id: i32, x: i32, y: i32, args: anytype) void {
     tic.spr(id, x * 8, y * 8, args);
+}
+
+fn offset(x: usize, y: usize) usize {
+    return x + ((y - 1) * 30);
+}
+
+export fn BOOT() void {
+    farm.start();
 }
 
 export fn TIC() void {
